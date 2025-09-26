a9fffad2a0ffe11687f7b70acd0416117f53f88e
bkmcm9205 <mcmillen.b@gmail.com>
2025-09-25 20:07:02 -0400

    Update strategy_registry.py

--- app/strategy_registry.py
+++ app/strategy_registry.py
@@ -2,4 +2,5 @@ ADAPTERS = {
     "trading_bot": "app.strategies.trading_bot_adapter",
     "ml_merged": "app.strategies.ml_merged_adapter",
     "ranked_ml": "app.strategies.ranked_ml_adapter",
+    "ml_sentiment": "app.strategies.ml_sentiment_adapter",
 }
