c780e2a868bedd3b499a8f14f8cfe7f1a21bd74f
bkmcm9205 <mcmillen.b@gmail.com>
2025-09-25 21:44:07 -0400

    Create trainer_registry.py

---
+++ app/trainer_registry.py
@@ -0,0 +1,6 @@
+TRAINERS = {
+    "trading_bot": "app.trainers.trading_bot_trainer",   # NEW
+    "ml_merged": "app.trainers.ml_merged_trainer",
+    "ranked_ml": "app.trainers.ranked_ml_trainer",
+    "ml_sentiment": "app.trainers.ml_sentiment_trainer",
+}
